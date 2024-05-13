92ba8d92d63e30dcfb4e6457d41da523f561c1b5
Otero <oteronickbrian@gmail.com>
2024-05-14 00:53:28 +0300

    scrapping jumia

--- scraper.go
+++ scraper.go
@@ -8,32 +8,63 @@ import (
 )
 
 // initializing a data structure to keep the scraped data
-type BasketballProduct struct {
+type PhoneProduct struct {
 	url, image, name, price string
 }
 
 func main() {
 	// initializing the slice of structs to store the data to scrape
-	var basketballProducts []BasketballProduct
+	var phoneProducts []PhoneProduct
 
 	// creating a new colly instance
 	c := colly.NewCollector()
 
 	// visiting the target page
-	c.Visit("https://hoopnation.co.ke/")
+	c.Visit("https://scrapeme.live/shop/")
 
 	// scraping logic
-	c.OnHTML("div.product", func(e *colly.HTMLElement) {
-		basketballProduct := BasketballProduct{}
+	c.OnHTML(".product", func(e *colly.HTMLElement) {
+		phoneProduct := PhoneProduct{}
 
-		basketballProduct.url = e.ChildAttr("a", "href")
-		basketballProduct.url = e.ChildAttr("img", "src")
-		basketballProduct.url = e.ChildText("h2")
-		basketballProduct.url = e.ChildText(".price")
+		phoneProduct.url = e.ChildAttr("a", "href")
+		phoneProduct.url = e.ChildAttr("img", "src")
+		phoneProduct.url = e.ChildText("h2")
+		phoneProduct.url = e.ChildText(".price")
 
-		basketballProducts = append(basketballProducts, basketballProduct)
+		phoneProducts = append(phoneProducts, phoneProduct)
 	})
 
-	
+	// opening the csv file
+	file, err := os.Create("products.csv")
+	if err != nil {
+		log.Fatalln("Failed to create output CSV file", err)
+	}
+	defer file.Close()
 
+	// initializing a file writer
+	writer := csv.NewWriter(file)
+
+	// writing the CSV headers
+	headers := []string{
+		"url",
+		"image",
+		"name",
+		"price",
+	}
+	writer.Write(headers)
+
+	// writing each basketball product as a CSV row
+	for _, phoneProduct := range phoneProducts {
+		// converting a basketballProduct to an array of strings
+		record := []string{
+			phoneProduct.url,
+			phoneProduct.image,
+			phoneProduct.name,
+			phoneProduct.price,
+		}
+
+		// writing the record to the CSV file
+		writer.Write(record)
+	}
+	defer writer.Flush()
 }
